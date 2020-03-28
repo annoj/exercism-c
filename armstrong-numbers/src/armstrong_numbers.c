78360816b728af820be74c12de4b03676e2da6fd
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-28 15:36:37 +0100

    armstrong-numbers: Refactor to use math.h functions

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -1,30 +1,12 @@
 #include "armstrong_numbers.h"
-
-static int power(int base, int exp)
-{
-	if (exp == 0) return 1;
-	else if (exp % 2) return base * power(base, exp - 1);
-	else {
-		int temp = power(base, exp / 2);
-		return temp * temp;
-	}
-}
+#include <math.h>
 
 int is_armstrong_number(int candidate)
 {
-	int n = candidate;
-	int p = 0;
-	while (n > 0) {
-		p++;
-		n = n / 10;	
-	}
-
+	int p = log10(candidate) + 1;
 	int acc = 0;
-	n = candidate;
-	while (n > 0) {
-		acc += power(n % 10, p); 
-		n = n / 10;
-	}
+	for (int n = candidate; n > 0; n /= 10)
+			acc += pow(n % 10, p);
 
 	return candidate == acc;
 }
