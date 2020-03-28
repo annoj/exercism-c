b9431ef05dc0e844422ec39244b7e9b4adc5e04d
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-28 18:12:20 +0100

    Add resistor-color-duo

---
+++ resistor-color-duo/src/resistor_color_duo.c
@@ -0,0 +1,7 @@
+#include "resistor_color_duo.h"
+#include <stdio.h>
+
+int color_code(resistor_band_t colors[])
+{
+	return colors[0] * 10 + colors[1];
+}
---
+++ resistor-color-duo/src/resistor_color_duo.h
@@ -0,0 +1,22 @@
+#ifndef RESISTOR_COLOR_DUO_H
+#define RESISTOR_COLOR_DUO_H
+
+#define NUM_COLORS 10
+
+#define BLACK	0
+#define BROWN	1
+#define RED		2
+#define ORANGE	3
+#define YELLOW	4
+#define GREEN	5
+#define BLUE	6
+#define VIOLET	7
+#define GREY	8
+#define WHITE 	9
+
+typedef int resistor_band_t;
+
+int color_code(resistor_band_t[]);
+
+
+#endif
