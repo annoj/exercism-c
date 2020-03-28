6e69961227d352d62980d0bf5e3b6045dd71790e
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-28 16:41:09 +0100

    armstrong-numbers: Handle log10() error cases

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -1,14 +1,16 @@
 #include "armstrong_numbers.h"
 #include <math.h>
+#include <stdbool.h>
 
 int is_armstrong_number(int candidate)
 {
+	// Handle Error cases of log10(),
+	// negative numbers are never armstrong numbers.
+	if (candidate == 0) return true;
+	if (candidate < 0) return false;
+
 	int p = log10(candidate) + 1;
 	int acc = 0;
-
-	// For candidate == 0 the loop is never executed, therefore
-	// acc == 0 and candidate == acc is true.
-	// This make the function resilient to log10(0) == -HUGE_VAL
 	for (int n = candidate; n > 0; n /= 10)
 			acc += pow(n % 10, p);
 
