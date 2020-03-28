9878b3002f42527f0a74cd510e07ddf13631fe0d
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-28 16:33:21 +0100

    armstrong-numbers: Add comment regarding log10(0) == -HUGE_VAL

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -5,6 +5,10 @@ int is_armstrong_number(int candidate)
 {
 	int p = log10(candidate) + 1;
 	int acc = 0;
+
+	// For candidate == 0 the loop is never executed, therefore
+	// acc == 0 and candidate == acc is true.
+	// This make the function resilient to log10(0) == -HUGE_VAL
 	for (int n = candidate; n > 0; n /= 10)
 			acc += pow(n % 10, p);
 
