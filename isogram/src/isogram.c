dde55ffe68deadd77df088b65ac6803acceaf6c1
Jona <s_heitzer@stud.hwr-berlin.de>
2020-03-31 21:22:00 +0200

    Add isogram

---
+++ isogram/src/isogram.c
@@ -0,0 +1,20 @@
+#include "isogram.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool is_isogram(const char phrase[])
+{
+	if (!phrase)
+			return false;
+
+	uint32_t letters = 0x0;
+	for (const char *p = phrase; *p; p++) {
+		if (*p == ' ' || *p == '-')
+				continue;
+		if (letters & (0x1 << (tolower(*p) - 0x61)))
+				return false;
+		letters ^= (0x1 << (tolower(*p) - 0x61));
+	}
+	return true;
+}
