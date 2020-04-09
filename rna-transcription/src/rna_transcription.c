8bf6c337b46f11e9f2dd573a3344b57f05614ef6
Jona <s_heitzer@stud.hwr-berlin.de>
2020-04-09 14:17:43 +0200

    rna-transcription: Make use of strlen to allocate rna buffer

--- rna-transcription/src/rna_transcription.c
+++ rna-transcription/src/rna_transcription.c
@@ -1,34 +1,32 @@
 #include "rna_transcription.h"
 #include <stdlib.h>
+#include <string.h>
 
 
 char *to_rna(const char *dna)
 {
-	char *rna = malloc(MAXLEN + 1);
-	int i = 0;
+	int slen = strlen(dna);
+	char *rna = malloc(slen + 1);
 	
-	for (const char *c = dna; *c; c++) {
-		if (i > MAXLEN)
-			return ERRLEN;
-
-		switch (*c) {
+	for (int i = 0; i < slen; i++) {
+		switch (dna[i]) {
 		case 'G':
-			rna[i++] = 'C';
+			rna[i] = 'C';
 			break;
 		case 'C':
-			rna[i++] = 'G';
+			rna[i] = 'G';
 			break;
 		case 'T':
-			rna[i++] = 'A';
+			rna[i] = 'A';
 			break;
 		case 'A':
-			rna[i++] = 'U';
+			rna[i] = 'U';
 			break;
 		default:
 			free(rna);
 			return NULL;
 		}
 	}
-	rna[i] = '\0';
+	rna[slen + 1] = '\0';
 	return rna;
 }
--- rna-transcription/src/rna_transcription.h
+++ rna-transcription/src/rna_transcription.h
@@ -1,9 +1,6 @@
 #ifndef RNA_TRANSCRIPTION_H
 #define RNA_TRANSCRIPTION_H
 
-#define MAXLEN 0xff
-#define ERRLEN NULL
-
 char *to_rna(const char *dna);
 
 #endif
